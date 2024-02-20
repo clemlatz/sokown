d699b1002bb3f65f79d597e0222e9d9a0f869119
Clément Latzarus <c@ltzr.io>
2024-02-20 23:17:17 +0100

    feat: add repository LocationRepository

---
+++ src/repositories/LocationRepository.ts
@@ -0,0 +1,19 @@
+import Location from "../models/Location";
+import Position from "../models/Position";
+
+const locations = [
+  new Location("earth", "Earth", new Position(1, 1)),
+  new Location("moon", "Moon", new Position(3, 3)),
+  new Location("mars", "Mars", new Position(23, 17)),
+];
+
+export default class LocationRepository {
+  getByCode(code: string): Location {
+    const location = locations.find(location => location.code === code);
+    if (!location) {
+      throw new Error(`Unknown location ${code}`);
+    }
+
+    return location;
+  }
+}
