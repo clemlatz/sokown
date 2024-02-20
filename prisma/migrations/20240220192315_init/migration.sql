f983c45337c5f583297b341e5906b75fbf733505
Clément Latzarus <c@ltzr.io>
2024-02-20 20:20:11 +0100

    build: add and configure prisma

---
+++ prisma/migrations/20240220192315_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Ship" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "currentPositionX" REAL NOT NULL,
+    "currentPositionY" REAL NOT NULL
+);
