b8db775bacd2ebde29d944b774f1cd5b86ffc507
Clément Latzarus <c@ltzr.io>
2024-02-21 20:19:07 +0100

    feat(server): enable CORS

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,6 @@ import { AppModule } from './app.module';
 
 export default async function server() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
