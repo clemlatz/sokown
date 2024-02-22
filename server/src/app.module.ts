d2fa0ed8eccefc513e9f8187969e889d2ce0396c
Clément Latzarus <c@ltzr.io>
2024-02-22 18:22:56 +0100

    feat(server): remove nextjs boilerplate code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ShipController } from './controllers/ShipController';
 import ShipRepository from './repositories/ShipRepository';
 import { PrismaClient } from '@prisma/client';
 
 @Module({
   imports: [],
-  controllers: [AppController, ShipController],
-  providers: [AppService, PrismaClient, ShipRepository],
+  controllers: [ShipController],
+  providers: [PrismaClient, ShipRepository],
 })
 export class AppModule {}
