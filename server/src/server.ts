f8aab48668035d6b4d4273b69e62749594ee3da3
Clément Latzarus <c@ltzr.io>
2024-02-23 16:34:47 +0100

    feat(server): allow server to parse JSON:API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
+
 import { AppModule } from './app.module';
 
 export default async function server() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
+
+  app.use(
+    bodyParser.json({
+      type: (req: any) =>
+        req.get('Content-Type') === 'application/vnd.api+json',
+      strict: false,
+    }),
+  );
+
   app.enableCors();
   await app.listen(3000);
 }
