0e8a751359b588609ce2a47ff0eb159a7780564f
Clément Latzarus <c@ltzr.io>
2024-03-01 20:30:34 +0100

    refactor(server): migrate controllers to typescript

--- client/app/controllers/ships/get.ts
+++ client/app/controllers/ships/get.ts
@@ -1,18 +1,20 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import Ship from 'sokown-client/models/ship';
 
 export default class ShipsGetController extends Controller {
-  @tracked newDestinationPositionX = 0;
-  @tracked newDestinationPositionY = 0;
+  @tracked newDestinationPositionX = '0';
+  @tracked newDestinationPositionY = '0';
+  declare model: Ship;
 
   @action
-  setDestination(event) {
+  async setDestination(event: Event) {
     event.preventDefault();
     this.model.destinationPosition = {
       x: parseFloat(this.newDestinationPositionX),
       y: parseFloat(this.newDestinationPositionY),
     };
-    this.model.save();
+    await this.model.save();
   }
 }
