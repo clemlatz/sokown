21de1e45d5396cca7876d94933985dea7466a766
Clément Latzarus <c@ltzr.io>
2024-03-01 20:52:40 +0100

    refactor(client): migrate routes to typescript

--- client/app/routes/ships/get.ts
+++ client/app/routes/ships/get.ts
@@ -1,17 +1,19 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import type Store from '@ember-data/store';
 
 import ENV from 'sokown-client/config/environment';
 
 export default class ShipsGetRoute extends Route {
-  @service store;
+  @service declare store: Store;
+  private autoRefresh: number = 0;
 
-  model(params) {
+  model(params: { id: number }) {
     return this.store.findRecord('ship', params.id);
   }
 
   afterModel() {
-    if (!this.autoRefresh && ENV.environment !== 'test') {
+    if (this.autoRefresh === 0 && ENV.environment !== 'test') {
       this.autoRefresh = setInterval(() => this.refresh(), 1000);
     }
   }
