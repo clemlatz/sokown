677722e0b325db56128f68f7c493bbfdac9de3bc
Clément Latzarus <c@ltzr.io>
2024-03-02 18:47:47 +0100

    feat(server): handle auth method without user in repo

--- server/src/repositories/UserRepository.test.ts
+++ server/src/repositories/UserRepository.test.ts
@@ -5,8 +5,53 @@ import UserRepository from './UserRepository';
 import { UnknownAuthenticationMethodError } from '../errors/UnknownAuthenticationMethodError';
 
 describe('UserRepository', () => {
-  describe('getById', () => {
-    describe('when user exists for given auth method id', () => {
+  describe('getByAuthenticationMethodId', () => {
+    describe('when there is no auth method for given id', () => {
+      it('throws an UnknownAuthenticationMethodError', async () => {
+        // given
+        const prisma = {
+          authenticationMethod: {
+            findUnique: jest.fn().mockResolvedValue(null),
+          },
+        } as unknown as PrismaClient;
+        const repository = new UserRepository(prisma);
+
+        // when
+        const promise = repository.getByAuthenticationMethodId(1);
+
+        // then
+        const expectedError = new UnknownAuthenticationMethodError(
+          'No authentication method found for given id',
+        );
+        await expect(promise).rejects.toThrow(expectedError);
+      });
+    });
+
+    describe('when auth method exists for given id but there is no user', () => {
+      it('throws an UnknownAuthenticationMethodError', async () => {
+        // given
+        const prisma = {
+          authenticationMethod: {
+            findUnique: jest.fn().mockResolvedValue({
+              id: 1,
+              user: null,
+            }),
+          },
+        } as unknown as PrismaClient;
+        const repository = new UserRepository(prisma);
+
+        // when
+        const promise = repository.getByAuthenticationMethodId(1);
+
+        // then
+        const expectedError = new UnknownAuthenticationMethodError(
+          'No user found for given authentication method id',
+        );
+        await expect(promise).rejects.toThrow(expectedError);
+      });
+    });
+
+    describe('when user and auth method exists for given id', () => {
       it('returns User', async () => {
         // given
         const prisma = {
@@ -32,27 +77,6 @@ describe('UserRepository', () => {
           where: { id: 1 },
           include: { user: true },
         });
-      });
-    });
-
-    describe('when user does not exist for given auth method id', () => {
-      it('throws an Error', async () => {
-        // given
-        const prisma = {
-          authenticationMethod: {
-            findUnique: jest.fn().mockResolvedValue(null),
-          },
-        } as unknown as PrismaClient;
-        const repository = new UserRepository(prisma);
-
-        // when
-        const promise = repository.getByAuthenticationMethodId(1);
-
-        // then
-        const expectedError = new UnknownAuthenticationMethodError(
-          'No authentication method found for given id',
-        );
-        await expect(promise).rejects.toThrow(expectedError);
       });
     });
   });
