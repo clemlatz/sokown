5f487ad38b22e056b99067a2a42da2e42ac95593
Clément Latzarus <c@ltzr.io>
2024-03-10 18:15:55 +0100

    feat(client): create user on signup form submit

--- client/tests/unit/controllers/user/signup-test.ts
+++ client/tests/unit/controllers/user/signup-test.ts
@@ -1,11 +1,41 @@
 import { module, test } from 'qunit';
+import sinon from 'sinon';
+
 import { setupTest } from 'sokown-client/tests/helpers';
+import type UserSignupController from 'sokown-client/controllers/user/signup';
 
 module('Unit | Controller | user/signup', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function (assert) {
-    const controller = this.owner.lookup('controller:user/signup');
-    assert.ok(controller);
+  module('register', function () {
+    test('it creates a new user', async function (assert) {
+      // given
+      const controller = this.owner.lookup(
+        'controller:user/signup',
+      ) as UserSignupController;
+      controller.set('pilotName', 'pilotName');
+      controller.set('shipName', 'shipName');
+      controller.set('notificationsAreEnabled', true);
+
+      const store = this.owner.lookup('service:store');
+      const userStub = { save: sinon.stub().resolves() };
+      const createRecordStub = sinon
+        .stub(store, 'createRecord')
+        .resolves(userStub);
+      const event = new Event('submit');
+
+      // when
+      await controller.register(event);
+
+      // then
+      assert.ok(
+        createRecordStub.calledWith('user', {
+          pilotName: 'pilotName',
+          shipName: 'shipName',
+          hasEnabledNotifications: true,
+        }),
+      );
+      assert.ok(userStub.save.calledOnce);
+    });
   });
 });
