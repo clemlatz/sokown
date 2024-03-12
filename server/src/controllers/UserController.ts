2f7a963657a786de3f60ddacaa133e99010af4d8
Clément Latzarus <c@ltzr.io>
2024-03-12 22:08:03 +0100

    feat(server): add UserController.create method

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,12 +1,60 @@
-import { Controller, Get, Res, Session, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Res,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
+
 import UserRepository from '../repositories/UserRepository';
 import AuthenticationGuard from '../guards/AuthenticationGuard';
 import SessionToken from '../models/SessionToken';
+import RegisterNewPilotUsecase from '../usescases/RegisterNewPilotUsecase';
+
+class UserToCreateDTO {
+  data: {
+    attributes: {
+      pilotName: string;
+      shipName: string;
+      hasEnabledNotifications: boolean;
+    };
+  };
+}
 
 @Controller()
 export default class UserController {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly registerNewPilotUsecase: RegisterNewPilotUsecase,
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  @Post('api/users')
+  async create(
+    @Session() session: SessionToken,
+    @Body() body: UserToCreateDTO,
+    @Res() res: Response,
+  ): Promise<void> {
+    const { attributes } = body.data;
+    await this.registerNewPilotUsecase.execute(
+      session.sub,
+      attributes.pilotName,
+      attributes.hasEnabledNotifications,
+      attributes.shipName,
+    );
+
+    res.send({
+      data: {
+        id: 'me',
+        type: 'user',
+        attributes: {
+          pilotName: attributes.pilotName,
+        },
+      },
+    });
+  }
 
   @Get('api/users/me')
   @UseGuards(AuthenticationGuard)
