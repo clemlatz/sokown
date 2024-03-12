88d104915f78dcce7e661176a2df2a0c183ccc69
Clément Latzarus <c@ltzr.io>
2024-03-12 22:08:03 +0100

    feat(server): add UserController.create method

--- server/src/controllers/UserController.test.ts
+++ server/src/controllers/UserController.test.ts
@@ -8,9 +8,15 @@ import UserRepository from '../repositories/UserRepository';
 import AuthenticationGuard from '../guards/AuthenticationGuard';
 import AuthenticationMethodRepository from '../repositories/AuthenticationMethodRepository';
 import SessionToken from '../models/SessionToken';
+import RegisterNewPilotUsecase from '../usescases/RegisterNewPilotUsecase';
+import ModelFactory from '../../test/ModelFactory';
+import ShipRepository from '../repositories/ShipRepository';
+import LocationRepository from '../repositories/LocationRepository';
 
 describe('UserController', () => {
   let userController: UserController;
+  let registerNewPilotUsecase: RegisterNewPilotUsecase;
+  let authenticationMethodRepository: AuthenticationMethodRepository;
   let userRepository: UserRepository;
 
   beforeEach(async () => {
@@ -19,13 +25,72 @@ describe('UserController', () => {
       providers: [
         PrismaClient,
         UserRepository,
+        ShipRepository,
+        LocationRepository,
         AuthenticationMethodRepository,
         AuthenticationGuard,
+        RegisterNewPilotUsecase,
       ],
     }).compile();
 
     userController = app.get<UserController>(UserController);
+    registerNewPilotUsecase = app.get<RegisterNewPilotUsecase>(
+      RegisterNewPilotUsecase,
+    );
+    authenticationMethodRepository = app.get<AuthenticationMethodRepository>(
+      AuthenticationMethodRepository,
+    );
     userRepository = app.get<UserRepository>(UserRepository);
+  });
+
+  describe('create', () => {
+    it('it registers a new pilot', async () => {
+      // given
+      const session = new SessionToken({ sub: 1 });
+      const authenticationMethod = ModelFactory.createAuthenticationMethod({
+        id: 1,
+        idTokenClaims: {
+          username: '',
+          email: 'sally@example.net',
+        },
+      });
+      jest
+        .spyOn(authenticationMethodRepository, 'getById')
+        .mockResolvedValue(authenticationMethod);
+      jest.spyOn(registerNewPilotUsecase, 'execute').mockResolvedValue();
+      const response = {
+        send: jest.fn(),
+      } as unknown as Response;
+      const payload = {
+        data: {
+          attributes: {
+            pilotName: 'Sally Ride',
+            hasEnabledNotifications: true,
+            shipName: 'STS-7 Challenger',
+          },
+        },
+      };
+
+      // when
+      await userController.create(session, payload, response);
+
+      // then
+      expect(registerNewPilotUsecase.execute).toHaveBeenCalledWith(
+        1,
+        'Sally Ride',
+        true,
+        'STS-7 Challenger',
+      );
+      expect(response.send).toHaveBeenCalledWith({
+        data: {
+          id: 'me',
+          type: 'user',
+          attributes: {
+            pilotName: 'Sally Ride',
+          },
+        },
+      });
+    });
   });
 
   describe('me', () => {
