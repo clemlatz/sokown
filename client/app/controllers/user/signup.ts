d22265efaa7dbc21236ea95584fb974864e8a148
Clément Latzarus <c@ltzr.io>
2024-04-10 12:39:56 +0200

    feat(client): display signup success message

--- client/app/controllers/user/signup.ts
+++ client/app/controllers/user/signup.ts
@@ -3,14 +3,19 @@ import type Store from '@ember-data/store';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+
 import type UserModel from 'sokown-client/models/user';
+import type CurrentUserService from 'sokown-client/services/current-user';
 
 export default class UserSignupController extends Controller {
   @service declare store: Store;
+  @service declare currentUser: CurrentUserService;
 
   @tracked pilotName: string = '';
   @tracked shipName: string = '';
   @tracked notificationsAreEnabled: boolean = false;
+
+  @tracked registrationSuccessful = false;
 
   @action
   async register(event: Event) {
@@ -22,5 +27,7 @@ export default class UserSignupController extends Controller {
       hasEnabledNotifications: this.notificationsAreEnabled,
     })) as UserModel;
     await user.save();
+
+    this.registrationSuccessful = true;
   }
 }
