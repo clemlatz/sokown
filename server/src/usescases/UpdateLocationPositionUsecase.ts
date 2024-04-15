d5807844c1aea7ac94a6586cf2748c9aa451f69a
Clément Latzarus <c@ltzr.io>
2024-04-15 20:30:14 +0200

    feat(web): update ship position with location's

--- server/src/usescases/UpdateLocationPositionUsecase.ts
+++ server/src/usescases/UpdateLocationPositionUsecase.ts
@@ -1,11 +1,22 @@
 import Location from '../models/Location';
 import AstronomyService from '../services/AstronomyService';
+import ShipRepository from '../repositories/ShipRepository';
 
 export default class UpdateLocationPositionUsecase {
-  constructor(private readonly astronomyService: AstronomyService) {}
+  constructor(
+    private readonly astronomyService: AstronomyService,
+    private readonly shipRepository: ShipRepository,
+  ) {}
 
   async execute(location: Location) {
     const position = await this.astronomyService.getPositionFor(location.code);
     location.setPosition(position);
+
+    const shipsAtPositions =
+      await this.shipRepository.getAllAtLocation(location);
+    for (const ship of shipsAtPositions) {
+      ship.currentPosition = location.position;
+      await this.shipRepository.update(ship);
+    }
   }
 }
