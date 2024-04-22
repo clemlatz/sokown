b8311047ca7e742e42e552b435c1c56dcbbc6cb2
Clément Latzarus <c@ltzr.io>
2024-04-22 21:52:55 +0200

    feat(server): convert sokown units to kilometers

--- server/src/values/DistanceInSokownUnits.ts
+++ server/src/values/DistanceInSokownUnits.ts
@@ -1,8 +1,14 @@
+import DistanceInKilometers from './DistanceInKilometers';
+
 export default class DistanceInSokownUnits {
   private readonly _value: number;
 
   get value(): number {
     return this._value;
+  }
+
+  get valueInKilometers(): DistanceInKilometers {
+    return new DistanceInKilometers(this._value / 0.00000668458);
   }
 
   constructor(value: number) {
