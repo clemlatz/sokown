10462c5c96ca3038b35f079758c7d4728b5e5ede
Clément Latzarus <c@ltzr.io>
2024-04-23 08:39:50 +0200

    test(client): isolate ship acceptance test

---
+++ client/tests/acceptance/ship-test.ts
@@ -0,0 +1,114 @@
+import { module, test } from 'qunit';
+import { currentURL, click, fillIn } from '@ember/test-helpers';
+import { visit } from '@1024pix/ember-testing-library';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import 'qunit-dom';
+import { Response } from 'miragejs';
+
+import { setupApplicationTest } from 'sokown-client/tests/helpers';
+
+type TestContext = {
+  server: {
+    get: (url: string, callback: () => Response) => void;
+  };
+};
+
+module('Acceptance | ship', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  module('visiting /ships/:id', function () {
+    test('it displays ship public information', async function (assert) {
+      // when
+      const screen = await visit('/');
+      await click(screen.getByRole('link', { name: 'Ships' }));
+      await click(screen.getByRole('link', { name: 'Artémis' }));
+
+      // then
+      assert.strictEqual(currentURL(), '/ships/1');
+      assert
+        .dom(screen.getByRole('heading', { name: 'Sokown', level: 1 }))
+        .exists();
+      assert
+        .dom(screen.getByRole('heading', { name: 'Artémis', level: 2 }))
+        .exists();
+      assert
+        .dom(screen.getByRole('definition', { name: 'Owner' }))
+        .hasText('Anne Morrow Lindbergh');
+      assert
+        .dom(screen.getByRole('definition', { name: 'Speed' }))
+        .hasText('100 km/s');
+      assert
+        .dom(screen.getByRole('definition', { name: 'Current location' }))
+        .hasText('3.000 3.000 Moon');
+      assert
+        .dom(screen.getByRole('definition', { name: 'Current destination' }))
+        .hasText('—');
+    });
+
+    module('when user is anonymous', function () {
+      test('it does not display autopilot form', async function (assert) {
+        // given
+        _userIsAnonymous(this as unknown as TestContext);
+
+        // when
+        const screen = await visit('/ships/1');
+
+        // then
+        assert.dom(screen.getByRole('link', { name: 'Log in' })).exists();
+        assert
+          .dom(screen.queryByRole('button', { name: 'Set Autopilot' }))
+          .doesNotExist();
+      });
+    });
+
+    module('when user is authenticated', function () {
+      test('it displays autopilot form', async function (assert) {
+        // when
+        const screen = await visit('/ships/1');
+
+        // then
+        assert.dom(screen.getByRole('textbox', { name: 'X' })).exists();
+        assert.dom(screen.getByRole('textbox', { name: 'Y' })).exists();
+        assert
+          .dom(screen.getByRole('button', { name: 'Set Autopilot' }))
+          .exists();
+      });
+    });
+  });
+
+  test('visiting /ships/:id and updating destination', async function (assert) {
+    // given
+    // @ts-expect-error missing type
+    this.server.create('ship', { id: 1 });
+
+    // when
+    const screen = await visit('/ships/1');
+    await fillIn(screen.getByRole('textbox', { name: 'X' }), '17');
+    await fillIn(screen.getByRole('textbox', { name: 'Y' }), '23');
+
+    await click(screen.getByRole('button', { name: 'Set Autopilot' }));
+    // then
+    assert.strictEqual(currentURL(), '/ships/1');
+    assert
+      .dom(screen.getByRole('definition', { name: 'Current destination' }))
+      .hasText('17.000 23.000');
+  });
+});
+
+function _userIsAnonymous(context: TestContext) {
+  context.server.get('/api/users/me', () => {
+    return new Response(
+      401,
+      {},
+      {
+        errors: [
+          {
+            status: 401,
+            title: 'Unauthorized',
+          },
+        ],
+      },
+    );
+  });
+}
--- client/tests/acceptance/sokown-test.js
+++ client/tests/acceptance/sokown-test.js
@@ -2,9 +2,8 @@ import { module, test } from 'qunit';
 import { currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'sokown-client/tests/helpers';
 import { visit } from '@1024pix/ember-testing-library';
-import { click, fillIn } from '@ember/test-helpers';
+import { click } from '@ember/test-helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import { Response } from 'miragejs';
 
 module('Acceptance | sokown', function (hooks) {
   setupApplicationTest(hooks);
@@ -42,83 +41,6 @@ module('Acceptance | sokown', function (hooks) {
     assert.dom(screen.getByRole('cell', { name: 'Bebop' })).exists();
   });
 
-  module('visiting /ships/:id', function () {
-    test('it displays ship public information', async function (assert) {
-      // when
-      const screen = await visit('/');
-      await click(screen.getByRole('link', { name: 'Ships' }));
-      await click(screen.getByRole('link', { name: 'Artémis' }));
-
-      // then
-      assert.strictEqual(currentURL(), '/ships/1');
-      assert
-        .dom(screen.getByRole('heading', { name: 'Sokown', level: 1 }))
-        .exists();
-      assert
-        .dom(screen.getByRole('heading', { name: 'Artémis', level: 2 }))
-        .exists();
-      assert
-        .dom(screen.getByRole('definition', { name: 'Owner' }))
-        .hasText('Anne Morrow Lindbergh');
-      assert
-        .dom(screen.getByRole('definition', { name: 'Speed' }))
-        .hasText('100 km/s');
-      assert
-        .dom(screen.getByRole('definition', { name: 'Current location' }))
-        .hasText('3.000 3.000 Moon');
-      assert
-        .dom(screen.getByRole('definition', { name: 'Current destination' }))
-        .hasText('—');
-    });
-
-    module('when user is anonymous', function () {
-      test('it does not display autopilot form', async function (assert) {
-        // given
-        _userIsAnonymous(this);
-
-        // when
-        const screen = await visit('/ships/1');
-
-        // then
-        assert.dom(screen.getByRole('link', { name: 'Log in' })).exists();
-        assert
-          .dom(screen.queryByRole('button', { name: 'Set Autopilot' }))
-          .doesNotExist();
-      });
-    });
-
-    module('when user is authenticated', function () {
-      test('it displays autopilot form', async function (assert) {
-        // when
-        const screen = await visit('/ships/1');
-
-        // then
-        assert.dom(screen.getByRole('textbox', { name: 'X' })).exists();
-        assert.dom(screen.getByRole('textbox', { name: 'Y' })).exists();
-        assert
-          .dom(screen.getByRole('button', { name: 'Set Autopilot' }))
-          .exists();
-      });
-    });
-  });
-
-  test('visiting /ships/:id and updating destination', async function (assert) {
-    // given
-    this.server.create('ship', { id: 1 });
-
-    // when
-    const screen = await visit('/ships/1');
-    await fillIn(screen.getByRole('textbox', { name: 'X' }), '17');
-    await fillIn(screen.getByRole('textbox', { name: 'Y' }), '23');
-
-    await click(screen.getByRole('button', { name: 'Set Autopilot' }));
-    // then
-    assert.strictEqual(currentURL(), '/ships/1');
-    assert
-      .dom(screen.getByRole('definition', { name: 'Current destination' }))
-      .hasText('17.000 23.000');
-  });
-
   test('visiting /user/profile', async function (assert) {
     // when
     const screen = await visit('/');
@@ -138,20 +60,3 @@ module('Acceptance | sokown', function (hooks) {
       .exists();
   });
 });
-
-function _userIsAnonymous(context) {
-  context.server.get('/api/users/me', () => {
-    return new Response(
-      401,
-      {},
-      {
-        errors: [
-          {
-            status: 401,
-            title: 'Unauthorized',
-          },
-        ],
-      },
-    );
-  });
-}
