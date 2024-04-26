e94f2077836a0e6b53ebdb13dc201d71257851dc
Clément Latzarus <c@ltzr.io>
2024-04-26 13:25:19 +0200

    feat(client): add ship.relativeTimeToArrival method

--- client/app/models/ship.ts
+++ client/app/models/ship.ts
@@ -1,8 +1,11 @@
 import Model, { attr } from '@ember-data/model';
-
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import type User from './user';
 import type Location from './location';
 import type { Position } from 'sokown-client/types';
+
+dayjs.extend(relativeTime);
 
 export default class ShipModel extends Model {
   @attr declare owner: User;
@@ -23,9 +26,22 @@ export default class ShipModel extends Model {
       return null;
     }
 
-    const nowInSeconds = Math.round(Date.now() / 1000);
-    const timeOfArrivalInSeconds = nowInSeconds + this.timeToDestination;
-    const timeOfArrival = new Date(timeOfArrivalInSeconds * 1000);
+    const timeOfArrival = this._getTimeOfArrival(this.timeToDestination);
     return timeOfArrival.toLocaleString();
+  }
+
+  get relativeTimeOfArrival(): string | null {
+    if (this.timeToDestination === null) {
+      return null;
+    }
+
+    const timeOfArrival = this._getTimeOfArrival(this.timeToDestination);
+    return dayjs().to(dayjs(timeOfArrival), true);
+  }
+
+  private _getTimeOfArrival(timeToDestination: number) {
+    const nowInSeconds = Math.round(Date.now() / 1000);
+    const timeOfArrivalInSeconds = nowInSeconds + timeToDestination;
+    return new Date(timeOfArrivalInSeconds * 1000);
   }
 }
