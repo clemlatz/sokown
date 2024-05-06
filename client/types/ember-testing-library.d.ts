0b6e49a61f2ca3b5791da8b0a16610f5d11d5d46
Clément Latzarus <c@ltzr.io>
2024-05-06 21:14:32 +0200

    feat(client): add StarMapObject component

--- client/types/ember-testing-library.d.ts
+++ client/types/ember-testing-library.d.ts
@@ -1,8 +1,9 @@
 declare module '@1024pix/ember-testing-library' {
+  import type { TemplateFactory } from 'ember-cli-htmlbars';
   export * from '@testing-library/dom/types';
 
   import { BoundFunctions } from '@testing-library/dom/types';
 
   export function visit(url: string): Promise<BoundFunctions>;
-  export function render(template: string): Promise<BoundFunctions>;
+  export function render(template: TemplateFactory): Promise<BoundFunctions>;
 }
