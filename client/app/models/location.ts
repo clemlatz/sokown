4ba99e588224c3b6bb6b5972f2d38e3b559d1b0c
Clément Latzarus <c@ltzr.io>
2024-05-08 19:29:37 +0200

    feat(client): add distanceFromTheSun property to Location

--- client/app/models/location.ts
+++ client/app/models/location.ts
@@ -3,6 +3,8 @@ import Model, { attr } from '@ember-data/model';
 import type { Position } from 'sokown-client/types';
 
 export default class Location extends Model {
-  @attr name: string | undefined;
-  @attr position: Position | undefined;
+  @attr declare name: string;
+  @attr declare position: Position;
+  @attr declare primaryBodyPosition: Position | null;
+  @attr declare distanceFromPrimaryBody: number;
 }
