60b8f982d28910be7f99a9ff93130eb78d383b51
Clément Latzarus <c@ltzr.io>
2024-05-09 09:58:54 +0200

    feat(client): display location with fill color

--- client/app/models/location.ts
+++ client/app/models/location.ts
@@ -4,6 +4,7 @@ import type { Position } from 'sokown-client/types';
 
 export default class Location extends Model {
   @attr declare name: string;
+  @attr declare color: string;
   @attr declare position: Position;
   @attr declare primaryBodyPosition: Position | null;
   @attr declare distanceFromPrimaryBody: number;
