8f1d892728ec5cab7ff93a8ebd6c16b2203e5db1
Clément Latzarus <c@ltzr.io>
2024-05-12 11:55:20 +0200

    feat(client): load locations on app startup

--- client/app/routes/application.ts
+++ client/app/routes/application.ts
@@ -2,11 +2,14 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 import type CurrentUserService from 'sokown-client/services/current-user';
+import type LocationsService from 'sokown-client/services/locations';
 
 export default class ApplicationRoute extends Route {
+  @service declare locations: LocationsService;
   @service declare currentUser: CurrentUserService;
 
   async model() {
+    await this.locations.load();
     await this.currentUser.load();
   }
 }
