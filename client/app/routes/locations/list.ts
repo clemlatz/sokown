a72a7dc23218fc6c9faaf410f2bcc77c46660050
Clément Latzarus <c@ltzr.io>
2024-05-12 11:56:23 +0200

    feat(client): get locations from locations service

--- client/app/routes/locations/list.ts
+++ client/app/routes/locations/list.ts
@@ -2,10 +2,13 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import type Store from '@ember-data/store';
 
+import type LocationsService from 'sokown-client/services/locations';
+
 export default class LocationsRoute extends Route {
   @service declare store: Store;
+  @service declare locations: LocationsService;
 
   async model() {
-    return this.store.findAll('location');
+    return this.locations.getAll();
   }
 }
