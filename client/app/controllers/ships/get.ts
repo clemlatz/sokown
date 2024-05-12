cae2f9bfa689a510b619933ea32b02634f32038e
Clément Latzarus <c@ltzr.io>
2024-05-12 13:15:00 +0200

    feat(client): display star map on ship page

--- client/app/controllers/ships/get.ts
+++ client/app/controllers/ships/get.ts
@@ -5,12 +5,20 @@ import { service } from '@ember/service';
 
 import Ship from 'sokown-client/models/ship';
 import type CurrentUserService from 'sokown-client/services/current-user';
+import type LocationsService from 'sokown-client/services/locations';
+import type Location from 'sokown-client/models/location';
 
 export default class ShipsGetController extends Controller {
   @service declare currentUser: CurrentUserService;
+  @service declare locations: LocationsService;
+
   declare model: Ship;
   @tracked newDestinationPositionX = '';
   @tracked newDestinationPositionY = '';
+
+  get starMapLocations(): Location[] {
+    return this.locations.getAll();
+  }
 
   @action
   async setDestination(event: Event) {
