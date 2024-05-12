ae6f28c6ac79106f9eb3ee50d5910074d99be036
Clément Latzarus <c@ltzr.io>
2024-05-12 13:04:13 +0200

    feat(client): allow to display ship on star map

--- client/tests/integration/components/star-map-test.ts
+++ client/tests/integration/components/star-map-test.ts
@@ -1,16 +1,32 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'sokown-client/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | star-map', function (hooks) {
   setupRenderingTest(hooks, {});
 
-  test('it renders', async function (assert) {
+  test('it displays locations and ship', async function (assert) {
+    // given
+    this.set('locations', [
+      {
+        name: 'Earth',
+        position: { x: 3, y: 4 },
+      },
+    ]);
+    this.set('ship', {
+      name: 'Artémis',
+      currentPosition: { x: 6, y: 7 },
+    });
+
     // when
-    await render(hbs`<StarMap />`);
+    const screen = await render(
+      hbs`<StarMap @locations={{this.locations}} @ship={{this.ship}} />`,
+    );
 
     // then
     assert.dom('text').hasText('300 S.U.');
+    assert.dom(screen.getByLabelText('Earth')).exists();
+    assert.dom(screen.getByLabelText('Artémis')).exists();
   });
 });
