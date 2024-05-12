efdfae21ac9af76c4dc40bec480d9483d3fe5b57
Clément Latzarus <c@ltzr.io>
2024-05-12 11:55:20 +0200

    feat(client): load locations on app startup

--- client/tests/unit/routes/application-test.js
+++ client/tests/unit/routes/application-test.js
@@ -5,16 +5,19 @@ import sinon from 'sinon';
 module('Unit | Route | application', function (hooks) {
   setupTest(hooks);
 
-  test('it loads current user', async function (assert) {
+  test('it loads current user and locations', async function (assert) {
     // given
     let route = this.owner.lookup('route:application');
     const currentUser = this.owner.lookup('service:current-user');
     sinon.stub(currentUser, 'load');
+    const locations = this.owner.lookup('service:locations');
+    sinon.stub(locations, 'load');
 
     // when
     await route.model();
 
     // then
     assert.true(currentUser.load.calledOnce);
+    assert.true(locations.load.calledOnce);
   });
 });
