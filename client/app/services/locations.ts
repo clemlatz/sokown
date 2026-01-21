76bf72f202330a25fa269c7f38619686fcb3b072
Clément Latzarus <c@ltzr.io>
2026-01-21 16:58:02 +0100

    feat(client): listen to live locations update
    by subscribe to server sent events

--- client/app/services/locations.ts
+++ client/app/services/locations.ts
@@ -6,16 +6,42 @@ import type Location from 'sokown-client/models/location';
 // noinspection JSUnusedGlobalSymbols
 export default class LocationsService extends Service {
   @service declare store: Store;
-  locations: Location[] = [];
+  @tracked locations: Location[] = [];
+  private locationSource: EventSource | null = null;
 
   async load(): Promise<void> {
     this.locations = (await this.store.findAll(
       'location',
     )) as unknown as Location[];
+
+    this.subscribe();
   }
 
   getAll(): Location[] {
     return this.locations;
+  }
+
+  subscribe(): void {
+    if (this.locationSource !== null) {
+      this.unsubscribe();
+    }
+
+    this.locationSource = new EventSource('/api/locations/live');
+
+    this.locationSource.addEventListener('message', (event) => {
+      const payload = JSON.parse(event.data);
+      this.store.push({ data: payload });
+    });
+  }
+
+  private unsubscribe() {
+    this.locationSource?.close();
+    this.locationSource = null;
+  }
+
+  willDestroy() {
+    super.willDestroy();
+    this.unsubscribe();
   }
 }
 
