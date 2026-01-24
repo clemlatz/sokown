cab678f33fba070d933bc3f4d4effc215cbc0f31
Clément Latzarus <c@ltzr.io>
2026-01-24 20:51:12 +0100

    feat: allow to set location proximity tolerance with a env var

--- server/src/helpers/isPositionWithinTolerance.ts
+++ server/src/helpers/isPositionWithinTolerance.ts
@@ -2,13 +2,16 @@ import Position from '../models/Position';
 import DistanceInSokownUnits from '../values/DistanceInSokownUnits';
 import calculateDistanceBetweenPositions from './calculateDistanceBetweenPositions';
 
-export const LOCATION_PROXIMITY_TOLERANCE = new DistanceInSokownUnits(0.1);
+function getLocationProximityTolerance() {
+  const valueFromEnv = process.env.LOCATION_PROXIMITY_TOLERANCE || '1';
+  const valueAsFloat = parseFloat(valueFromEnv);
+  return new DistanceInSokownUnits(valueAsFloat);
+}
 
 export default function isPositionWithinTolerance(
   position1: Position,
   position2: Position,
-  tolerance: DistanceInSokownUnits = LOCATION_PROXIMITY_TOLERANCE,
 ): boolean {
   const distance = calculateDistanceBetweenPositions(position1, position2);
-  return distance.value <= tolerance.value;
+  return distance.value <= getLocationProximityTolerance().value;
 }
